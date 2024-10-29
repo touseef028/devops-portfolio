eac9843d2eb241c437c23ba2a8901aaf36da2d40
tauseef028 <touseef.afzalch@gmail.com>
2024-10-29 16:15:54 +0000

    Updates

---
+++ precache-manifest.71da4f531360fe8ee245c4a91dcf0d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6be04213d52b7bbb7d93f0d81aa76cc",
+    "url": "/devops-portfolio/index.html"
+  },
+  {
+    "revision": "a697b3340fd834ce808b",
+    "url": "/devops-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "183dd76d817e04e60824",
+    "url": "/devops-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "a697b3340fd834ce808b",
+    "url": "/devops-portfolio/static/js/2.9c83e434.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/devops-portfolio/static/js/2.9c83e434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183dd76d817e04e60824",
+    "url": "/devops-portfolio/static/js/main.9566d3fa.chunk.js"
+  },
+  {
+    "revision": "62e9811b7005b0901ddb",
+    "url": "/devops-portfolio/static/js/runtime-main.0c0c90f5.js"
+  }
+]);

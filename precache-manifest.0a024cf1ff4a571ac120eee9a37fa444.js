c170db71302780e9c7376db82e6ad6dc2cbb0ab9
Tauseef Afzal <touseef.afzalch@gmail.com>
2025-07-25 02:30:31 +0100

    Updates

---
+++ precache-manifest.0a024cf1ff4a571ac120eee9a37fa444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254f1a26ecb4ad3e9cdf6092160ee87a",
+    "url": "/devops-portfolio/index.html"
+  },
+  {
+    "revision": "e5d067f6c9a3e1614a5e",
+    "url": "/devops-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f7b890049ae14c19a654",
+    "url": "/devops-portfolio/static/css/main.9ed9e0d6.chunk.css"
+  },
+  {
+    "revision": "e5d067f6c9a3e1614a5e",
+    "url": "/devops-portfolio/static/js/2.22a8b245.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/devops-portfolio/static/js/2.22a8b245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b890049ae14c19a654",
+    "url": "/devops-portfolio/static/js/main.fab180aa.chunk.js"
+  },
+  {
+    "revision": "62e9811b7005b0901ddb",
+    "url": "/devops-portfolio/static/js/runtime-main.0c0c90f5.js"
+  }
+]);
